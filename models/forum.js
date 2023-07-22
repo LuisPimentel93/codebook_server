55c027dc4e780c419c480c6de7b948aa12f2469a
Luis Pimentel <luispimentel@Luiss-MBP.lan>
2023-07-21 22:21:25 -0400

    edited vercel file

--- models/forum.js
+++ models/forum.js
@@ -1,19 +1,19 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-const forumSchema = new mongoose.Schema({
-    getsUpdates: {
-      type: Boolean,
-      default: false
-    },
-    ProfileName: {
-      type: String,
-      default: false
-    },
-    Body: {
-      type: String,
-      default: false
-    } 
-},{
-  timestamps: true
-})
-module.exports = mongoose.model('Forum', forumSchema)
+// const forumSchema = new mongoose.Schema({
+//     getsUpdates: {
+//       type: Boolean,
+//       default: false
+//     },
+//     ProfileName: {
+//       type: String,
+//       default: false
+//     },
+//     Body: {
+//       type: String,
+//       default: false
+//     } 
+// },{
+//   timestamps: true
+// })
+// module.exports = mongoose.model('Forum', forumSchema)
